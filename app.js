81e5b3fe90392c44398c832f6fdfd8d00205d70a
Kunj Chauhan <kunjchauhan27@gmail.com>
2025-11-22 23:26:41 +0530

    Styling Index Done

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); //setting index.ejs
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
