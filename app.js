d7a653fa0387e81de5cc7f9a014d906ac6440229
Kunj Chauhan <kunjchauhan27@gmail.com>
2025-11-27 14:55:17 +0530

    Added Login Signup via Passport

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 //---------------------------------------------------------------------------------------
 //DataBase Connections
@@ -52,6 +56,15 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -60,8 +73,19 @@ app.use((req, res, next) => {
 });
 //---------------------------------------------------------------------------------------
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "brainspirebaroda@gmail.com",
+    username: "brains27",
+  });
+
+  let registeredUser = await User.register(fakeUser, "Kunj@27122004");
+  res.send(registeredUser);
+});
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
 //---------------------------------------------------------------------------------------
 //Sever Side Validation
 
